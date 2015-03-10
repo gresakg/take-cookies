a512961c3ef668bc1aeedb211d7f80ce4a7c79d8
Gregor G <design@gresak.net>
2015-03-10 22:05:15 +0100

    demo ready

--- takecookies.js
+++ takecookies.js
@@ -1,8 +1,14 @@
 var noco=jQuery.cookie('nocook');var nocostyle="<style>\
 #askcook { \
 background-color: #ffffff; \
-padding: 1px;\n\
-border-bottom: 2px solid #343434; \
+border: 2px solid #343434; \
+bottom: 20px; \
+max-height: 30%; \
+padding: 1px; \
+position: absolute; \
+right: 20px; \
+width: 300px; \
+display: none; \
 } \
 #askcook a, #askcook a:visited, #askcook a:hover {\
 color: #31a8f0; \
@@ -18,5 +24,10 @@ button#cookok { \
 display:block; \
 margin: 0 auto; \
 } \
-</style>";if(!noco){jQuery("head").append(nocostyle);jQuery("body").prepend("<div id='askcook'><p>To spletno mesto uporablja piškotke z namenom zagotavljanja spletne storitve, oglasnih sistemov in funkcionalnosti, ki jih brez piškotkov ne bi mogli nuditi. Z obiskom in uporabo spletnega mesta soglašate s piškotki. <span><a href='http://zavod.over.net/o-piskotkih/'>Več o piškotkih</a> </span> <button id='cookok'>V redu</button></p></div>");jQuery("#cookok").bind('click',function(){jQuery.cookie('nocook','1',{expires:365});jQuery('#askcook').hide('slow');});}
+</style>";if(!noco){
+	jQuery("head").append(nocostyle);
+	jQuery("body").prepend("<div id='askcook'><p>To spletno mesto uporablja piškotke z namenom zagotavljanja spletne storitve, oglasnih sistemov in funkcionalnosti, ki jih brez piškotkov ne bi mogli nuditi. Z obiskom in uporabo spletnega mesta soglašate s piškotki. <span><a href='http://zavod.over.net/o-piskotkih/'>Več o piškotkih</a> </span> <button id='cookok' class='pure-button'>V redu</button></p></div>");
+	jQuery("#askcook").fadeIn('slow');
+	jQuery("#cookok").bind('click',function(){jQuery.cookie('nocook','1',{expires:365});
+	jQuery('#askcook').fadeOut('slow');});}
 else{}
